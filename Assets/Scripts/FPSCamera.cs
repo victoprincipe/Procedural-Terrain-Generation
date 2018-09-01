1ee9d65ccf91150b30cac42671e0de6b1a2c30e9
Runewather <victo_10_@hotmail.com>
2018-09-01 18:33:07 -0300

    Simple FPS Camera fix

--- Assets/Scripts/FPSCamera.cs
+++ Assets/Scripts/FPSCamera.cs
@@ -6,18 +6,22 @@ public class FPSCamera : MonoBehaviour {
 
 	[SerializeField]
 	private float mouseSensitivity;
+
+    [SerializeField]
+    private float maxVertical;
 	
 	void UpdateRotation ()
     {
         float horizontal = Input.GetAxis ("Mouse X") * mouseSensitivity;
         float vertical = Input.GetAxis ("Mouse Y") * mouseSensitivity;
-        vertical = Mathf.Clamp (vertical, -80, 80);
  
         transform.Rotate (0, horizontal, 0);
-        //Camera.main.transform.localRotation = Quaternion.Euler(vertical, 0, 0);
+        float lastCameraAngle = Camera.main.transform.eulerAngles.x;
+        float newCameraAngle = lastCameraAngle - vertical;
+        Camera.main.transform.eulerAngles = new Vector3(newCameraAngle, transform.eulerAngles.y, 0);
     }
 
-	void FixedUpdate () {
+	void Update () {
 		UpdateRotation();
 	}
 }
