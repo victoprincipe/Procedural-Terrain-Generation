0dc769f25778260c50c96a5c00fbbe72b770e129
Runewather <victo_10_@hotmail.com>
2018-09-09 13:29:23 -0300

    Random Rock Script

---
+++ Assets/Scripts/RandomRockScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRockScript : MonoBehaviour {
+    [SerializeField]
+    private Vector2 randomSizeRange;
+
+    void Start()
+    {
+        float rand = Random.Range(randomSizeRange.x , randomSizeRange.y);
+        transform.localScale = new Vector3(rand, rand, rand);
+        //transform.eulerAngles = new Vector3(rand * transform.position.x, rand * transform.position.y, rand * transform.position.z);
+    }
+	
+}
